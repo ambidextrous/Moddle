684d3dc972079a746139481a69fa6e6fb40b01f0
David Skillman <david.skillman@hotmail.com>
2017-02-16 11:05:22 +0000

    url patterns added and barebones views added

--- moddle_project/moddle/views.py
+++ moddle_project/moddle/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/index.html', context=context_dict)
+
+def user_profile(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/user_profile.html', context=context_dict)
+
+def login(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/login.html', context=context_dict)
+
+def register(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/register.html', context=context_dict)
+
+def search(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/search.html', context=context_dict)
+
+def upload_bike(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/upload_bike.html', context=context_dict)
+
+def bike_profile(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/bike_profile.html', context=context_dict)
+
+def request_bike(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/request_bike.html', context=context_dict)
+
+def view_bookings(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/view_bookings.html', context=context_dict)
+
+def faq(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/faq.html', context=context_dict)
+
+def contact_us(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/contact_us.html', context=context_dict)
+
+def about(request):
+    context_dict = {'': ''}
+    return render(request, 'moddle/about.html', context=context_dict)
--- moddle_project/moddle_project/urls.py
+++ moddle_project/moddle_project/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from moddle import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_name_slug>[\w\-]+)/$', views.index, name='user_profile'),
+    url(r'^(?P<user_name_slug>[\w\-]+)/mybookings/$', views.view_bookings, name='view_bookings'),
+    url(r'^(?P<user_name_slug>[\w\-]+)/addbike/$', views.upload_bike, name='upload_bike'),
+    url(r'^(?P<bike_id_slug>[\w\-]+)/$', views.bike_profile, name='bike_profile'),
+    url(r'^(?P<bike_id_slug>[\w\-]+)/request/$', views.request_bike, name='request_bike'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^contact_us$', views.contact_us, name='contact_us'),
+    url(r'^faq$', views.faq, name='faq'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
 ]
+
