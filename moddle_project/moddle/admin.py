521a8338b0ff8ea88754880deca1f886405999fd
David Skillman <david.skillman@hotmail.com>
2017-02-28 11:06:22 +0000

    updated admin.py and models.py

--- moddle_project/moddle/admin.py
+++ moddle_project/moddle/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from moddle.models import User, Bike
+#from moddle.models import Booking
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'full_name', 'post_code')
+
+class BikeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'owner')
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Bike, BikeAdmin)
--- moddle_project/moddle/models.py
+++ moddle_project/moddle/models.py
@@ -5,40 +5,40 @@ from django.contrib.auth.models import User
 import datetime
 
 class User(models.Model):
-	username = models.CharField(max_length=128, unique=True)
-	full_name = models.CharField(max_length=128)
-	email = models.EmailField(max_length=128)
-	phone_number = models.CharField(max_length=16, blank=True)
-	gender_male = models.BooleanField()
-	post_code = models.CharField(max_length=7)
+    username = models.CharField(max_length=128, unique=True)
+    full_name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=128)
+    phone_number = models.CharField(max_length=16, blank=True)
+    gender_male = models.BooleanField()
+    post_code = models.CharField(max_length=7)
 
-	def __str__(self):
-		return self.username
-	# for python 2.7
-	def __unicode__(self):
-		return self.username
+    def __str__(self):
+        return self.username
+    # for python 2.7
+    def __unicode__(self):
+        return self.username
 
 class Bike(models.Model):
-	name = models.CharField(max_length=128)
-	owner = models.ForeignKey(User)
-	boys_bike = models.BooleanField()
-	adults_bike = models.BooleanField()
-	description = models.CharField(max_length=512, unique=True)
-	bike_picture = models.ImageField()
-	bike_picture = models.ImageField(upload_to='profile_images', blank=True)
+    name = models.CharField(max_length=128)
+    owner = models.ForeignKey(User)
+    boys_bike = models.BooleanField()
+    adults_bike = models.BooleanField()
+    description = models.CharField(max_length=512, unique=True)
+    #bike_picture = models.ImageField()
+    bike_picture = models.ImageField(upload_to='profile_images', blank=True)
 
-	def __str__(self):
-		return self.name
-	# for python 2.7
-	def __unicode__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+    # for python 2.7
+    def __unicode__(self):
+        return self.name
 
 #class Booking(models.Model):
-#	start_date = models.DateField()
-#	finish_date = models.DateField()
-#	owner = models.ForeignKey(User)
-#	borrower = models.ForeignKey(User)
-#	bikeid = models.ForeignKey(Bike)
+#    start_date = models.DateField()
+#    finish_date = models.DateField()
+#    owner = models.ForeignKey(User)
+#    borrower = models.ForeignKey(User)
+#    bikeid = models.ForeignKey(Bike)
 #
-#	def save(self, *args, **kwargs):
-#		super(Booking, self).save(*args, **kwargs)
+#    def save(self, *args, **kwargs):
+#        super(Booking, self).save(*args, **kwargs)
