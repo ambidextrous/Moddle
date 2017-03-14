2eca21b4be9a77c3db13e57544fdab7b732e2f1c
Chih-Wei Hsieh <2283402h@student.gla.ac.uk>
2017-03-14 14:38:34 +0000

    Search function.. still working on

--- moddle_project/moddle/views.py
+++ moddle_project/moddle/views.py
@@ -20,17 +20,17 @@ def get_user_object(request):
 def index(request):
     # Request the context of the request
     # The context contains information of all available bikes
-    filter_category = request.GET.getlist('category')
-    bike_list = []
-    if filter_category:
-        for cat in filter_category:
-            if cat=='boys':
-                bike_list = Bike.objects.filter(boys_bike=True)
-            if cat=='adults':
-                temp = Bike.objects.filter(adults_bike=True)
-                bike_list += temp
-    else:
-        bike_list = Bike.objects.order_by()
+
+    if request.method == 'GET':
+        try:
+            bike_gender = request.GET.get('bike_gender')
+            bike_age = request.GET.get('bike_age')
+            category = request.GET.get('category')
+            bike_list = Bike.objects.filter(bike_gender=bike_gender, bike_age=bike_age, category=category)
+            if not bike_list:
+                bike_list = Bike.objects.order_by()
+        except Bike.DoesNotExist:
+            print 'Cannot find'
     context_dict = {'bikes':bike_list}
     return render(request, 'moddle/index.html', context=context_dict)
 
